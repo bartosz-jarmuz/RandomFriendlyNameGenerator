832f2a028f25cb52ae48c7954636ab60228bde5e
Bartosz Jarmuz <bjarmuz@sdl.com>
2019-07-27 22:02:08 +0200

    add project

---
+++ src/RandomFriendlyNameGenerator/Generator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomFriendlyNameGenerator
+{
+    public class Generator
+    {
+        private static Random firstPartRandom = new Random();
+        private static Random secondPartRandom = new Random();
+        private static Random thirdPartRandom = new Random();
+
+        public string GetName()
+        {
+            return $"{Adjectives.Values[firstPartRandom.Next()]}{FemaleFirstNames.Values[firstPartRandom.Next()]}";
+        }
+    }
+}
